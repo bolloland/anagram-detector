dad62ff31597af95854754cd20e75329fe7ce91d
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-25 13:43:18 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+        def initialize(word)
+            @word = word
+        end
+          
+        def match(possible)
+            #new_array = []
+            possible.select do |a|
+                #binding.pry
+                if a.split("").sort == @word.split("").sort
+                    a
+                end
+                end
+                # possible
+        end
+
+end
